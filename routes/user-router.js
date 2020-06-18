ca457786af056aad7f352a9db41913665499fe8f
IsaiahFlax <flax.isaiah@gmail.com>
2020-06-17 23:40:18 -0600

    get users

--- routes/user-router.js
+++ routes/user-router.js
@@ -14,7 +14,11 @@ router.post('/register', (req, res) => {
 })
 
 router.get('/users', (req, res) => {
-    Helpers.getAll
+    Helpers.getAll.then(user => res.status(200).json(user))
+    .catch(err => {
+        res.status(500).json({message: `Error getting users`})
+    }
+    )
 })
 
 module.exports = router
