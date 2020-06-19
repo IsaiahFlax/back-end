dbda98656458272ddaaabb5dca9264b1b08da95e
IsaiahFlax <flax.isaiah@gmail.com>
2020-06-18 19:20:25 -0600

    added getproducts

--- routes/user-router.js
+++ routes/user-router.js
@@ -48,7 +48,7 @@ router.get('/users', (req, res) => {
 })
 
 router.get('/products', (req, res) => {
-    Helpers.getProducts.then(products => {res.status(200).json(products)})
+    Helpers.getProducts().then(products => {res.status(200).json(products)})
     .catch(err => {
         res.status(500).json({message: `Error getting products`})
     })
