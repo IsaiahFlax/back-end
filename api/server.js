7d32e009101789618c129aaba575b0578f7bbe13
IsaiahFlax <flax.isaiah@gmail.com>
2020-06-18 19:53:47 -0600

    added helmet and cors

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 
 const userRouter = require('../routes/user-router.js')
 
 const server = express()
 
+server.use(helmet())
+server.use(cors())
 server.use(express.json())
 
 server.get('/', (req, res) => {
