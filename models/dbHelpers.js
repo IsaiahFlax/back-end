bf9daba4f0bb4d1fb0a3a9b5100badb18314397d
IsaiahFlax <flax.isaiah@gmail.com>
2020-06-22 21:42:18 -0600

    i want to return all of the users, including the jsut registered user when a new user in registered!

--- models/dbHelpers.js
+++ models/dbHelpers.js
@@ -52,7 +52,7 @@ function findById(id) {
 }
 
 async function getAllUsers() {
-    return await db('users').select('users.username', 'users.firstname', 'users.lastname', 'users.email', 'users.id');
+    return await db('users').select('users.username', 'users.firstname', 'users.lastname', 'users.email', 'users.id').orderBy('users.id', 'desc');
  }
 
 function getProducts() {
